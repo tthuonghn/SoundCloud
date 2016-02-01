31a15b5b6f10875a87e498031ceda4d145c52a30
Trung Đức <ducnt1409@gmail.com>
2016-02-01 22:56:50 +0700

    fix next, back button

---
+++ SoundCloudNew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoundCloudNew
+//
+//  Created by Trung Đức on 1/24/16.
+//  Copyright © 2016 Trung Đức. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
